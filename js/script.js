6452c7e9aebf77a290969846a13c66b92fb1d92b
ken-phu-nti-johanneberg <ken.phu@elev.ntig.se>
2021-05-28 11:26:23 +0200

    nya sidor, hamburgermeny

--- js/script.js
+++ js/script.js
@@ -2,7 +2,11 @@ function toggleMeny() {
 
     document.querySelector('.meny_list').classList.toggle('navActive')
     document.querySelector('.burger').classList.toggle('toggle')
-
+    document.querySelector('body').classList.toggle('scroll')
 }
 
 document.querySelector('.burger').addEventListener('click', toggleMeny)
+
+// Lägg till i produkt sidan en funktion som tillåter en att välja hur många man vill köpa bredvid "lägg i varukorg"
+
+//     background-position: ; ändrar positionen av bilden. Center verkar bra
